b8481b5bb09536c7cafebcf515becf21b9b21a78
SHABIN-K <helloshabink@gmail.com>
2025-11-06 14:43:23 +0530

    Temp rename for Netlify case issue

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,11 @@
-import AppLoader from '@/components/AppLoader'
+import AppLoaderFix from '@/components/AppLoaderFix'
 import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
     return (
         <div className="relative bg-[#f3f3f3] min-h-screen">
-            <Suspense fallback={<AppLoader />}>
+            <Suspense fallback={<AppLoaderFix />}>
                 <Outlet />
             </Suspense>
         </div>
